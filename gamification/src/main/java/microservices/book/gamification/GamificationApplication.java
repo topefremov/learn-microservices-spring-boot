ca6a30ec36afb62e4dc723cc377cb75281d53cfd
Aleksandr Efremov <topefremov@yandex.ru>
2018-01-07 22:21:00 +0300

    Add service-registry and gateway

--- gamification/src/main/java/microservices/book/gamification/GamificationApplication.java
+++ gamification/src/main/java/microservices/book/gamification/GamificationApplication.java
@@ -2,8 +2,10 @@ package microservices.book.gamification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class GamificationApplication {
 
 	public static void main(String[] args) {
--- gateway/src/main/java/microservices/book/gateway/GatewayApplication.java
+++ gateway/src/main/java/microservices/book/gateway/GatewayApplication.java
@@ -1,14 +1,16 @@
-package microservices.book.multiplication;
+package microservices.book.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 @EnableEurekaClient
-public class SocialMultiplicationApplication {
+public class GatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SocialMultiplicationApplication.class, args);
+		SpringApplication.run(GatewayApplication.class, args);
 	}
 }
